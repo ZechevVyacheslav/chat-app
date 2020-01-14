1be7584355844bdaa7bdb7ce9de04d34ecca0f12
ZechevVyacheslav <zhechevslavik@gmail.com>
2020-01-14 23:15:37 +0200

    getting and adding messages

--- src/models/infrastructure/repository/MessageRepository.ts
+++ src/models/infrastructure/repository/MessageRepository.ts
@@ -1,12 +1,31 @@
 import { EntityRepository, Repository } from 'typeorm';
 import IMessageRepository from '../../domain/interfaces/IMessageRepository';
 import Message from '../../domain/core/Message';
+import User from '../../domain/core/User';
+
+const cutUserPasswordFromMessage = (message: Message) => {
+  const userWithoutPassword = new User();
+  userWithoutPassword.id = message.user.id;
+  userWithoutPassword.username = message.user.username;
+  userWithoutPassword.email = message.user.email;
+
+  const updatedMessage = new Message();
+  updatedMessage.id = message.id;
+  updatedMessage.text = message.text;
+  updatedMessage.room = message.room;
+  updatedMessage.user = userWithoutPassword;
+  return updatedMessage;
+};
 
 @EntityRepository(Message)
 export default class MessageRepositroy extends Repository<Message>
   implements IMessageRepository {
   addMessage(message: Message) {
-    return this.save(message);
+    return this.save(message)
+      .then(savedMessage => {
+        return this.findOne(savedMessage.id, { relations: ['room', 'user'] });
+      })
+      .then(populatedMessage => cutUserPasswordFromMessage(populatedMessage));
   }
 
   getMessageById(messageId: number) {
@@ -14,8 +33,13 @@ export default class MessageRepositroy extends Repository<Message>
   }
 
   getMessagesByRoomId(roomId: number) {
-    return this.find({ relations: ['room'] }).then(messages => {
-      return messages.filter(message => message.room.id === roomId);
+    return this.find({ relations: ['room', 'user'] }).then(messages => {
+      const filteredMessages = messages.filter(
+        message => message.room.id === roomId
+      );
+      return filteredMessages.map(message => {
+        return cutUserPasswordFromMessage(message);
+      });
     });
   }
 
